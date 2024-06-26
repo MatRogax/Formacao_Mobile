9fc5bfb1d8af048f0a403a6b3290aa6fb33239bb
Matheus Rogato <matheusrogato@gmail.com>
2024-06-26 20:06:45 -0300

    adcionando arquitetura no projeto

--- finances/lib/main.dart
+++ finances/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:finances/models/transaction.dart';
+import 'package:finances/pages/transaction_user.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(FinanceApp());
@@ -14,104 +14,36 @@ class FinanceApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  final transactions = [
-    Transaction(
-        id: 't1',
-        title: 'tenis de corrida',
-        value: 320.87,
-        date: DateTime.now()),
-    Transaction(
-        id: 't2',
-        title: 'maquina de lavar',
-        value: 880.50,
-        date: DateTime.now()),
-  ];
-
   HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 0, 0, 2),
+      backgroundColor: const Color.fromARGB(255, 0, 0, 2),
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255,0, 0, 2),
+        backgroundColor: const Color.fromARGB(255, 0, 0, 2),
         foregroundColor: Colors.white,
-        title: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 25),
-            child: Text(
-              'Finance App'),
-          ),
+        title: const Padding(
+          padding: EdgeInsets.only(top: 25),
+          child: Text('Finance App'),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          
-          Column(
-            children: transactions.map((tr) {
-              return Card(
-                shape:ContinuousRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                color: Color.fromARGB(255,13, 13, 13),
-              
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 10),
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 51, 49, 49),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: const Icon(
-                        Icons.currency_bitcoin_sharp,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(top: 20, right: 0),
-                          child: Text(
-                            tr.title,
-                            style: const TextStyle(
-                              color:Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 175),
-                          child: Text(
-                            '-\$${tr.value.toStringAsFixed(2)}', // formata com 2 casas decimais
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w800,
-                              color: Color.fromARGB(255, 252, 252, 252),
-                            ),
-                          ),
-                        ),
-                        Center(
-                          child: Text(
-                            tr.date.toString(),
-                            style: const TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromARGB(255, 55, 55, 55),
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          )
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              "Histórico",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          TransactionUser(),
         ],
       ),
     );
